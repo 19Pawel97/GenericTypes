3fbcc0d9d2b49ec83c149fa20d1304c8fb754751
19Pawel97 <19pawel9772@gmail.com>
2021-05-28 14:47:39 +0200

    Generic Method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,7 +19,17 @@ public class Main {
             System.out.println(pairOfShoes.getLeft().toString() + " " + pairOfShoes.getRight().toString());
         }
 
+        if (isShoe(pair.getLeft()) && isShoe(pair.getRight())) {
+            System.out.println("We've got a pair of shoes!");
+        }
+    }
 
+    public static <T> boolean isShoe(T object) {
+        if (object.getClass() == Shoe.class) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
